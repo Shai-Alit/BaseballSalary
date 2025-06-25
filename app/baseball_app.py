0de0435f043a5e74dfb18791c4e40c673874ba05
git <sean.ford@sas.com>
2025-06-25 17:40:01 +0000

    updates

--- app/baseball_app.py
+++ app/baseball_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import settings
+from PIL import Image
 
 st.write('Hello World.')
+
+image_footer = Image.open(settings.img_loc + '/SAS_logo.png')
+st.image(image_footer,caption='Powered by SAS',width=100)
