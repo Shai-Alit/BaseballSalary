e524f9434d32056f24ea6a91566197f9435cde7c
git <sean.ford@sas.com>
2025-06-25 17:18:56 +0000

    added settings

---
+++ app/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 12:27:07 2025
+
+@author: seford
+"""
+import os
+
+if os.path.exists('./img'):
+    img_loc = './img'
+else:
+    img_loc = '../img'
+
+if os.path.exists('./models'):
+    pickle_path = './models'
+else:
+    pickle_path = '../models'
